381c7c384cb441766f8d1e86fcbefa2152422ffb
daruanugerah <anugerahdaru@gmail.com>
2021-06-22 00:15:05 +0700

    add header on homepage

--- lib/pages/main_page/home_page.dart
+++ lib/pages/main_page/home_page.dart
@@ -1,10 +1,58 @@
+import 'package:bwa_ecom_prov/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    //note: header
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Row(
+          children: [
+            // NAME and USERNAME
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hallo, Alex',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 24, fontWeight: semiBold),
+                  ),
+                  Text(
+                    '@alexkeinn',
+                    style: subtitleTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // AVATAR
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/image_profile.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
