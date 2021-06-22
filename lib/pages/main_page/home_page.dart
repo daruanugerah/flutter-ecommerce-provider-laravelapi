e7eaad2c035cf412f7dd6f8f60091aec013b2c65
daruanugerah <anugerahdaru@gmail.com>
2021-06-22 20:18:34 +0700

    slicing product tile new arrivals on homepage

--- lib/pages/main_page/home_page.dart
+++ lib/pages/main_page/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:bwa_ecom_prov/theme.dart';
 import 'package:bwa_ecom_prov/widgets/product_card.dart';
+import 'package:bwa_ecom_prov/widgets/product_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -212,12 +213,45 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget newArrivalsTitle() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Text(
+          'New Arrivals',
+          style: primaryTextStyle.copyWith(
+            fontSize: 22,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    Widget newArrivalsCard() {
+      return Container(
+        margin: EdgeInsets.only(top: 14),
+        child: Column(
+          children: [
+            ProductTile(),
+            ProductTile(),
+            ProductTile(),
+            ProductTile(),
+          ],
+        ),
+      );
+    }
+
     return ListView(
       children: [
         header(),
         categories(),
         popularProductsTitle(),
         popularProductsCard(),
+        newArrivalsTitle(),
+        newArrivalsCard(),
       ],
     );
   }
