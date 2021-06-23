0fc8e8e65d3d6698486e5297ef4b3fde4d61c5ee
daruanugerah <anugerahdaru@gmail.com>
2021-06-24 00:53:40 +0700

    slicing wishlist page

--- lib/pages/main_page/wishlist_page.dart
+++ lib/pages/main_page/wishlist_page.dart
@@ -1,10 +1,101 @@
+import 'package:bwa_ecom_prov/theme.dart';
+import 'package:bwa_ecom_prov/widgets/wishlist_card.dart';
 import 'package:flutter/material.dart';
 
 class WishlistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Wishlist Page'),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text('Favorite Shoes'),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget emptyWishlist() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/image_wishlist.png',
+                width: 74,
+              ),
+              SizedBox(
+                height: 23,
+              ),
+              Text(
+                'You don\'t have a dream shoes?',
+                style:
+                    primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Let\'s find yout favorite shoes',
+                style: secondaryTextStyle,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 44,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Explore Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ),
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 24,
+                    ),
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          color: backgroundColor3,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+            children: [
+              WishlistCard(),
+              WishlistCard(),
+              WishlistCard(),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        //emptyWishlist(),
+        content(),
+      ],
     );
   }
 }
