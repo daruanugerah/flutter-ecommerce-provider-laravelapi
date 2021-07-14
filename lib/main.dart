b23183629c87386ab4a5f6f24cfe9e1aeaad017d
daruanugerah <anugerahdaru@gmail.com>
2021-07-14 18:23:44 +0700

    add firebase settings for andro and ios

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,13 @@ import 'package:bwa_ecom_prov/providers/cart_provider.dart';
 import 'package:bwa_ecom_prov/providers/product_provider.dart';
 import 'package:bwa_ecom_prov/providers/transaction_provider.dart';
 import 'package:bwa_ecom_prov/providers/wishlist_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
